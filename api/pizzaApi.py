9b0f1939a39516443764a35dc02b8fe8e88ba96f
Yaron Torgeman <yaron.torgeman@cyberark.com>
2023-08-09 22:19:50 +0300

    add helms chart

--- api/pizzaApi.py
+++ api/pizzaApi.py
@@ -87,5 +87,4 @@ def order():
         return jsonify(error="Internal server error"), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
