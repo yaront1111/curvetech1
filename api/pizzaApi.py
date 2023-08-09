ef842896c9157b15d424387a14086125756e4ee1
Yaron Torgeman <yaron.torgeman@cyberark.com>
2023-08-10 00:28:23 +0300

    reorgenise code

--- api/pizzaApi.py
+++ api/pizzaApi.py
@@ -1,34 +1,41 @@
 from flask import Flask, request, jsonify
 import pika
 import jwt
+from pymongo import MongoClient
 from jwt.exceptions import InvalidTokenError
 import os
 
+
+# Configuration
+class Config:
+    SERVICE_SECRET_KEY = os.environ.get("SERVICE_SECRET_KEY")
+    MONGO_USERNAME = os.environ.get('MONGODB_USERNAME', 'user')
+    MONGO_PASSWORD = os.environ.get('MONGODB_PASSWORD', 'password')
+    MONGO_URL = f'mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@pizza-ordering-mongodb:27017/pizza_db'
+    RABBITMQ_USERNAME = os.environ.get("RABBITMQ_USERNAME", "user")
+    RABBITMQ_PASSWORD = os.environ.get("RABBITMQ_PASSWORD", "password")
+
+
+# Application setup
 app = Flask(__name__)
+app.config.from_object(Config)
 
-SERVICE_SECRET_KEY = os.environ.get("SERVICE_SECRET_KEY")
-RABBITMQ_URL = os.environ.get("RABBITMQ_URL", "pizza-ordering-rabbitmq")
-RABBITMQ_USERNAME = os.environ.get("RABBITMQ_USERNAME", "user")
-RABBITMQ_PASSWORD = os.environ.get("RABBITMQ_PASSWORD", "password")
 
-@app.route("/health", methods=["GET"])
-def health():
-    db_status = check_database_connection()
-    queue_status = check_message_queue()
-    overall_status = "OK" if db_status and queue_status else "ERROR"
-    return jsonify(status=overall_status, database=db_status, queue=queue_status), 200
-
+# Database and queue utilities
 def check_database_connection():
     try:
+        client = MongoClient(app.config['MONGO_URL'])
+        client.admin.command("ismaster")
         return "OK"
     except Exception as e:
         app.logger.error(f"Database check failed: {e}")
         return "ERROR"
 
+
 def check_message_queue():
     try:
-        credentials = pika.PlainCredentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD)
-        connection_params = pika.ConnectionParameters(RABBITMQ_URL, credentials=credentials)
+        credentials = pika.PlainCredentials(app.config['RABBITMQ_USERNAME'], app.config['RABBITMQ_PASSWORD'])
+        connection_params = pika.ConnectionParameters(app.config['RABBITMQ_URL'], credentials=credentials)
         connection = pika.BlockingConnection(connection_params)
         connection.close()
         return "OK"
@@ -36,57 +43,72 @@ def check_message_queue():
         app.logger.error(f"Message queue check failed: {e}")
         return "ERROR"
 
+
+# Token and order utilities
 def verify_service_token(token):
     try:
-        # Verify the token using JWT
-        jwt.decode(token, SERVICE_SECRET_KEY, algorithms=["HS256"])
+        jwt.decode(token, app.config['SERVICE_SECRET_KEY'], algorithms=["HS256"])
         return True
     except InvalidTokenError:
         return False
 
+
 def add_order_to_queue(order):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_URL))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=app.config['RABBITMQ_URL']))
     channel = connection.channel()
     channel.queue_declare(queue='pizza_orders')
     channel.basic_publish(exchange='', routing_key='pizza_orders', body=str(order))
     connection.close()
 
+
+# Routes
+@app.route("/health", methods=["GET"])
+def health():
+    db_status = check_database_connection()
+    queue_status = check_message_queue()
+    overall_status = "OK" if db_status and queue_status else "ERROR"
+    return jsonify(status=overall_status, database=db_status, queue=queue_status), 200
+
+
 @app.route("/order", methods=["POST"])
 def order():
-    try:
-        auth_header = request.headers.get("Authorization")
-        token = auth_header.split(" ")[1] if auth_header else None
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000, debug=True)
 
-        if not verify_service_token(token):
-            return jsonify(error="Unauthorized"), 401
+        try:
+            auth_header = request.headers.get("Authorization")
+            token = auth_header.split(" ")[1] if auth_header else None
 
-        data = request.json
-        pizza_type = data.get("pizza-type")
-        size = data.get("size")
-        amount = data.get("amount")
+            if not verify_service_token(token):
+                return jsonify(error="Unauthorized"), 401
 
-        allowed_pizza_types = ["margherita", "pugliese", "marinara"]
-        allowed_sizes = ["personal", "family"]
+            data = request.json
+            pizza_type = data.get("pizza-type")
+            size = data.get("size")
+            amount = data.get("amount")
 
-        if (
-            not all([pizza_type, size, amount])
-            or pizza_type not in allowed_pizza_types
-            or size not in allowed_sizes
-            or amount < 1
-        ):
-            return jsonify(error="Invalid input"), 400
+            allowed_pizza_types = ["margherita", "pugliese", "marinara"]
+            allowed_sizes = ["personal", "family"]
 
-        order_details = {
-            "pizza_type": pizza_type,
-            "size": size,
-            "amount": amount
-        }
-        add_order_to_queue(order_details)
+            if (
+                not all([pizza_type, size, amount])
+                or pizza_type not in allowed_pizza_types
+                or size not in allowed_sizes
+                or amount < 1
+            ):
+                return jsonify(error="Invalid input"), 400
 
-        return jsonify(status="Order received"), 200
-    except Exception as e:
-        app.logger.error(f"Order processing failed: {e}")
-        return jsonify(error="Internal server error"), 500
+            order_details = {
+                "pizza_type": pizza_type,
+                "size": size,
+                "amount": amount
+            }
+            add_order_to_queue(order_details)
+
+            return jsonify(status="Order received"), 200
+        except Exception as e:
+            app.logger.error(f"Order processing failed: {e}")
+            return jsonify(error="Internal server error"), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- order_queue/queue_worker.py
+++ order_queue/queue_worker.py
@@ -4,45 +4,46 @@ import json
 import os
 from pymongo import MongoClient
 
+# Configuration
+class Config:
+    RABBITMQ_URL = os.environ.get("RABBITMQ_URL", "pizza-ordering-rabbitmq")
+    RABBITMQ_USERNAME = os.environ.get("RABBITMQ_USERNAME", "user")
+    RABBITMQ_PASSWORD = os.environ.get("RABBITMQ_PASSWORD", "password")
+    MONGO_USERNAME = os.environ.get('MONGODB_USERNAME', 'user')
+    MONGO_PASSWORD = os.environ.get('MONGODB_PASSWORD', 'password')
+    MONGO_URL = f'mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@pizza-ordering-mongodb:27017/pizza_db'
+
+
+# Logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-RABBITMQ_URL = os.environ.get("RABBITMQ_URL", "pizza-ordering-rabbitmq")
-RABBITMQ_USERNAME = os.environ.get("RABBITMQ_USERNAME", "user")
-RABBITMQ_PASSWORD = os.environ.get("RABBITMQ_PASSWORD", "password")
-MONGO_USERNAME = os.environ.get('MONGODB_USERNAME', 'user')
-MONGO_PASSWORD = os.environ.get('MONGODB_PASSWORD', 'password')
-MONGO_URL = f'mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@pizza-ordering-mongodb:27017/pizza_db'
-client = MongoClient(MONGO_URL)
+# Database connection
+client = MongoClient(Config.MONGO_URL)
 db = client['pizza_db']
 orders_collection = db['orders']
 
-def process_order(order_message):
 
+# Order processing
+def process_order(order_message):
     try:
+        # Extract order details
         pizza_type = order_message['pizza-type']
         size = order_message['size']
         amount = order_message['amount']
         logger.info(f"Processing order: {pizza_type} - {size} - {amount}")
 
-        # Connect to MongoDB
-        client = MongoClient(MONGO_URL)
-        db = client.pizza_db
-        collection = db.orders
-
-        # Insert the order into the MongoDB collection
-        order_data = {
-            "pizza_type": pizza_type,
-            "size": size,
-            "amount": amount
-        }
-        result = collection.insert_one(order_data)
+        # Insert the order into MongoDB
+        order_data = {"pizza_type": pizza_type, "size": size, "amount": amount}
+        result = orders_collection.insert_one(order_data)
         logger.info(f"Order inserted into MongoDB with ID: {result.inserted_id}")
 
     except Exception as e:
         logger.error(f"Failed to process order: {e}")
         raise
 
+
+# RabbitMQ Callback
 def callback(ch, method, properties, body):
     try:
         order_message = json.loads(body)
@@ -50,16 +51,16 @@ def callback(ch, method, properties, body):
         ch.basic_ack(delivery_tag=method.delivery_tag)
     except Exception as e:
         logger.error(f"Failed to process message: {e}")
-        #TODO dead-letter queue
+        # TODO: dead-letter queue
 
+
+# Main function
 def main():
-    credentials = pika.PlainCredentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD)
-    connection_params = pika.ConnectionParameters(RABBITMQ_URL, credentials=credentials)
+    credentials = pika.PlainCredentials(Config.RABBITMQ_USERNAME, Config.RABBITMQ_PASSWORD)
+    connection_params = pika.ConnectionParameters(Config.RABBITMQ_URL, credentials=credentials)
     connection = pika.BlockingConnection(connection_params)
     channel = connection.channel()
-
     channel.queue_declare(queue='pizza_orders')
-
     channel.basic_consume(queue='pizza_orders', on_message_callback=callback)
 
     logger.info('Waiting for messages. To exit press CTRL+C')
